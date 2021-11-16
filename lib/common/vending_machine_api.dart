b3a487cc8eb38588ff38a8a88f3996fc7ddbaf5b
Ravenen <redstoyningbros@gmail.com>
2021-11-16 19:39:08 +0200

    Moved API call logic into the separate class

---
+++ lib/common/vending_machine_api.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:flutter_async_app/common/constants.dart';
+import 'package:flutter_async_app/common/vending_machine.dart';
+import 'package:http/http.dart' as http;
+
+class VendingMachineApi {
+  static const String _baseUrl = Constants.apiUrlBase;
+  static const String _path = "/vending_machine";
+
+  static Future<List<VendingMachine>> getAll() async {
+    final response = await http.get(Uri.parse("$_baseUrl$_path"));
+    if (response.statusCode == 200) {
+      List data = List.from(jsonDecode(response.body));
+      List<VendingMachine> vendingMachineList =
+          data.map((item) => VendingMachine.fromJson(item)).toList();
+      return vendingMachineList;
+    } else {
+      throw Exception('Failed to load vending machines');
+    }
+  }
+}
--- lib/screens/vending_machines_page/vending_machines_cards_list.dart
+++ lib/screens/vending_machines_page/vending_machines_cards_list.dart
@@ -1,48 +1,24 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_async_app/common/vending_machine.dart';
+import 'package:flutter_async_app/common/vending_machine_api.dart';
 import 'package:flutter_async_app/screens/vending_machines_page/vending_machine_card.dart';
-import 'package:http/http.dart' as http;
 
-
-class VendingMachinesCardsList extends StatefulWidget {
+class VendingMachinesCardsList extends StatelessWidget {
   const VendingMachinesCardsList({Key? key}) : super(key: key);
-
-  @override
-  _VendingMachinesCardsListState createState() =>
-      _VendingMachinesCardsListState();
-}
-
-class _VendingMachinesCardsListState extends State<VendingMachinesCardsList> {
-  late Future<List<VendingMachine>> futureVendingMachineList;
-
-  @override
-  void initState() {
-    super.initState();
-    futureVendingMachineList = fetchVendingMachines();
-  }
-
-  Future<List<VendingMachine>> fetchVendingMachines() async {
-    final response = await http.get(Uri.parse("https://e4c577p993.execute-api.us-east-2.amazonaws.com/dev/vending_machine/"));
-    List data = List.from(jsonDecode(response.body));
-    List<VendingMachine> vendingMachineList =
-        data.map((item) => VendingMachine.fromJson(item)).toList();
-    return Future.delayed(const Duration(seconds: 5), () => vendingMachineList);
-  }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<VendingMachine>>(
-        future: futureVendingMachineList,
+        future: VendingMachineApi.getAll(),
         builder: (context, snapshot) {
           if (snapshot.hasData && snapshot.data!.isNotEmpty) {
             return Expanded(
               child: ListView.builder(
                 itemCount: snapshot.data!.length,
                 itemBuilder: (context, index) {
-                  return VendingMachineCard(vendingMachine: snapshot.data![index]);
+                  return VendingMachineCard(
+                      vendingMachine: snapshot.data![index]);
                 },
               ),
             );
@@ -52,8 +28,7 @@ class _VendingMachinesCardsListState extends State<VendingMachinesCardsList> {
 
           return const Padding(
             padding: EdgeInsets.all(50.0),
-            child: Center(
-                child: Text("Loading...")),
+            child: Center(child: Text("Loading...")),
           );
         });
   }
