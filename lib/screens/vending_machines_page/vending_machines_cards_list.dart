c33c9b30d82c450d0ac905af19a6dcb82a2f5cb6
Ravenen <redstoyningbros@gmail.com>
2021-11-25 01:27:30 +0200

    Added the bloc support for the api calls

--- lib/screens/vending_machines_page/vending_machines_cards_list.dart
+++ lib/screens/vending_machines_page/vending_machines_cards_list.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_async_app/common/data/vending_machine.dart';
-import 'package:flutter_async_app/common/vending_machine_api.dart';
+import 'package:flutter_async_app/common/bloc/vending_machines_bloc.dart';
+import 'package:flutter_async_app/common/bloc/vending_machines_event.dart';
+import 'package:flutter_async_app/common/bloc/vending_machines_state.dart';
 import 'package:flutter_async_app/screens/loading_text.dart';
 import 'package:flutter_async_app/screens/vending_machines_page/vending_machine_card.dart';
 
@@ -10,24 +11,33 @@ class VendingMachinesCardsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<VendingMachine>>(
-        future: VendingMachineApi.getAll(),
+    VendingMachinesBloc _vendingMachinesBloc = VendingMachinesBloc();
+    _vendingMachinesBloc.vendingMachinesEventSink
+        .add(VendingMachinesLoadEvent());
+
+    return StreamBuilder<VendingMachinesState>(
+        stream: _vendingMachinesBloc.vendingMachinesStateStream,
         builder: (context, snapshot) {
-          if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+          if (snapshot.data is VendingMachinesLoadedState) {
             return Expanded(
               child: ListView.builder(
-                itemCount: snapshot.data!.length,
+                itemCount: (snapshot.data as VendingMachinesLoadedState)
+                    .vendingMachines
+                    .length,
                 itemBuilder: (context, index) {
                   return VendingMachineCard(
-                      vendingMachine: snapshot.data![index]);
+                      vendingMachine:
+                          (snapshot.data as VendingMachinesLoadedState)
+                              .vendingMachines[index]);
                 },
               ),
             );
+          } else if (snapshot.data is VendingMachinesLoadingState) {
+            return const LoadingText();
           } else if (snapshot.hasError) {
             return Text('${snapshot.error}');
           }
-
-          return const LoadingText();
+          return const Text("Undefined");
         });
   }
 }
